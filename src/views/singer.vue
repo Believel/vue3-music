c9b616b2cd11e086d18b83d40e1c9b03209f8c3d
believel <zjrzpp0414@163.com>
2023-10-31 19:49:56 +0800

    获取歌手列表接口

--- src/views/singer.vue
+++ src/views/singer.vue
@@ -1,7 +1,26 @@
 <script setup>
+import { reactive, onBeforeMount } from 'vue'
+import { getSingerList } from '@/service/singer'
+
+const data = reactive({
+  singers: []
+})
+onBeforeMount(async () => {
+  const result = await getSingerList()
+  data.singers = result.singers
+})
 </script>
 <template>
-  <div>
+  <div class="singer" v-loading="!data.singers.length">
     歌手页面
   </div>
 </template>
+
+<style lang="scss" scoped>
+.singer {
+  position: fixed;
+  width: 100%;
+  top: 88px;
+  bottom: 0;
+}
+</style>

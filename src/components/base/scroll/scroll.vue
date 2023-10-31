a673d2ddcad917fd0d536d14c4e9a724faefde0d
believel <zjrzpp0414@163.com>
2023-10-31 11:54:50 +0800

    scroll

---
+++ src/components/base/scroll/scroll.vue
@@ -0,0 +1,19 @@
+<script setup>
+import useScroll from './use-scroll'
+import { defineProps, ref } from 'vue'
+
+const rootRef = ref(null)
+const props = defineProps({
+  click: {
+    type: Boolean,
+    default: true
+  }
+})
+useScroll(rootRef, props)
+</script>
+
+<template>
+  <div ref="rootRef">
+    <slot></slot>
+  </div>
+</template>
--- src/views/recommend.vue
+++ src/views/recommend.vue
@@ -1,6 +1,7 @@
 <script setup>
 import { reactive, onBeforeMount } from 'vue'
 import Slider from '@/components/base/slider/slider'
+import Scroll from '@/components/base/scroll/scroll'
 import { getRecommend } from '@/service/recommend'
 
 const data = reactive({
@@ -16,33 +17,37 @@ onBeforeMount(async () => {
 </script>
 <template>
   <div class="recommend">
-    <div class="slider-wrapper">
-      <div class="slider-content">
-        <Slider :sliders="data.sliders" v-if="data.sliders.length"></Slider>
+    <Scroll class="recommend-content">
+      <div>
+        <div class="slider-wrapper">
+        <div class="slider-content">
+          <Slider :sliders="data.sliders" v-if="data.sliders.length"></Slider>
+        </div>
+        </div>
+        <div class="recommend-list">
+          <h1 class="list-title">热门歌单推荐</h1>
+          <ul>
+            <li
+              v-for="item in data.albums"
+              class="item"
+              :key="item.id"
+            >
+              <div class="icon">
+                <img width="60" height="60" :src="item.pic" />
+              </div>
+              <div class="text">
+                <h2 class="name">
+                  {{ item.username }}
+                </h2>
+                <p class="title">
+                  {{ item.title }}
+                </p>
+              </div>
+            </li>
+          </ul>
+        </div>
       </div>
-    </div>
-    <div class="recommend-list">
-      <h1 class="list-title">热门歌单推荐</h1>
-      <ul>
-        <li
-          v-for="item in data.albums"
-          class="item"
-          :key="item.id"
-        >
-          <div class="icon">
-            <img width="60" height="60" :src="item.pic" />
-          </div>
-          <div class="text">
-            <h2 class="name">
-              {{ item.username }}
-            </h2>
-            <p class="title">
-              {{ item.title }}
-            </p>
-          </div>
-        </li>
-      </ul>
-    </div>
+    </Scroll>
   </div>
 </template>
 <style lang="scss" scoped>
@@ -51,55 +56,61 @@ onBeforeMount(async () => {
     width: 100%;
     top: 88px;
     bottom: 0;
-    .slider-wrapper {
-      position: relative;
-      width: 100%;
-      height: 0;
-      padding-top: 40%;
+    overflow: scroll;
+    .recommend-content {
+      height: 100%;
       overflow: hidden;
-      .slider-content {
-        position: absolute;
-        left: 0;
-        top: 0;
+      .slider-wrapper {
+        position: relative;
         width: 100%;
-        height: 100%;
-      }
-    }
-    .recommend-list {
-      .list-title {
-        height: 65px;
-        line-height: 65px;
-        text-align: center;
-        font-size: $font-size-medium;
-        color: $color-theme;
-      }
-      .item {
-        display: flex;
-        box-sizing: border-box;
-        align-items: center;
-        padding: 0 20px 20px 20px;
-        .icon {
-          flex: 0 0 60px;
-          width: 60px;
-          padding-right: 20px;
+        height: 0;
+        padding-top: 40%;
+        overflow: hidden;
+        .slider-content {
+          position: absolute;
+          left: 0;
+          top: 0;
+          width: 100%;
+          height: 100%;
         }
-        .text {
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          flex: 1;
-          line-height: 20px;
-          overflow: hidden;
+      }
+      .recommend-list {
+        .list-title {
+          height: 65px;
+          line-height: 65px;
+          text-align: center;
           font-size: $font-size-medium;
-          .name {
-            margin-bottom: 10px;
-            color: $color-text;
+          color: $color-theme;
+        }
+        .item {
+          display: flex;
+          box-sizing: border-box;
+          align-items: center;
+          padding: 0 20px 20px 20px;
+          .icon {
+            flex: 0 0 60px;
+            width: 60px;
+            padding-right: 20px;
           }
-          .title {
-            color: $color-text-d;
+          .text {
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+            flex: 1;
+            line-height: 20px;
+            overflow: hidden;
+            font-size: $font-size-medium;
+            .name {
+              margin-bottom: 10px;
+              color: $color-text;
+            }
+            .title {
+              color: $color-text-d;
+            }
           }
         }
       }
     }
+
   }
 </style>

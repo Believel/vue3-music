8d44a33a757294fcb7db1dc0fa064a2d458e63b4
believel <zjrzpp0414@163.com>
2023-11-08 20:19:44 +0800

    限制滑动方向

--- src/components/player/use-middle-interactive.js
+++ src/components/player/use-middle-interactive.js
@@ -15,12 +15,26 @@ export default function useMiddleInteractive () {
 
   function onMiddleTouchStart (e) {
     touch.x = e.touches[0].pageX
+    touch.y = e.touches[0].pageY
+    touch.directionLocked = ''
   }
   function onMiddleTouchMove (e) {
-    const delta = e.touches[0].pageX - touch.x
+    const deltaX = e.touches[0].pageX - touch.x
+    const deltaY = e.touches[0].pageY - touch.y
+
+    const absDeltaX = Math.abs(deltaX)
+    const absDeltaY = Math.abs(deltaY)
+    // 通过滑动的 x 偏移量和 y 偏移量的多少来确定在哪个方向滑动
+    if (!touch.directionLocked) {
+      touch.directionLocked = absDeltaX >= absDeltaY ? 'h' : 'v'
+    }
+    // 如果是 Y 方向就什么也不做
+    if (touch.directionLocked === 'v') {
+      return
+    }
     const left = currentView === 'cd' ? 0 : -window.innerWidth
     // 滑动宽度：最大不能超过屏幕宽度
-    const offsetWidth = Math.min(0, Math.max(-window.innerWidth, left + delta))
+    const offsetWidth = Math.min(0, Math.max(-window.innerWidth, left + deltaX))
     touch.percent = Math.abs(offsetWidth / window.innerWidth)
     if (currentView === 'cd') {
       if (touch.percent > 0.2) {
